3b3184cb064d475a52ed28ae464e2ee7f8dc38f2
rajm688 <90443069+rajm688@users.noreply.github.com>
2021-10-20 16:08:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+// //Set up Http Request
+// var xhr = new XMLHttpRequest();
+
+// //create and send Get request
+// //GET,POST,PUT,DELETE,
+// // Second =>ENdpoint of the url:server url
+// xhr.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+// // set up our listener to process the request
+// xhr.onload = function () {
+//   //process the request and server will return the data
+//   if (xhr.status >= 200 && xhr.status < 300) {
+//     var t = JSON.parse(this.response);
+//     // console.log(t);
+//     // document.write(t);
+//     for(i=0;i<t.length;i++){
+//     console.log(t[i].name);
+//     console.log(t[i].population);
+//     console.log(t[i].region);
+//     console.log(t[i].currency);
+//   }
+//   } else {
+//     //it should not give you data
+//     console.log("Data is not available");
+//     // document.write(t);
+//   }
+// };
+
+// xhr.send();
 //Set up Http Request
 var xhr = new XMLHttpRequest();
 
 //create and send Get request
 //GET,POST,PUT,DELETE,
 // Second =>ENdpoint of the url:server url
-xhr.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
 // set up our listener to process the request
 xhr.onload = function () {
   //process the request and server will return the data
   if (xhr.status >= 200 && xhr.status < 300) {
     var t = JSON.parse(this.response);
-    // console.log(t);
-    // document.write(t);
+   //console.log(t);
+    //document.write(t);
     for(i=0;i<t.length;i++){
-    console.log(t[i].name);
-    console.log(t[i].population);
-    console.log(t[i].region);
-    console.log(t[i].currency);
-  }
+    console.log(`name : ${t[i].name}
+                email_ID : ${t[i].email}
+                Address : ${t[i]["address"].city}
+                phone : ${t[i].phone}`);
+ }
   } else {
     //it should not give you data
     console.log("Data is not available");
@@ -26,3 +54,4 @@ xhr.onload = function () {
 };
 
 xhr.send();
+
