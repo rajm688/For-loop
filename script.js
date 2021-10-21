ca436cdac548563df397ce36c7607e979860e2eb
rajm688 <90443069+rajm688@users.noreply.github.com>
2021-10-21 16:47:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,23 @@ class product{
 let product1 = new product("p1", "name 1", "100");
 let product2 = new product("p2", "name 2", "200");
 let product3 = new product("p3", "name 3", "300");
-let arr = [product1, product2, product3]
+let arr = [product1, product2, product3];
 for(i=0;i<arr.length;i++){
     console.log(arr[i]);
 }
 console.log("__________________________");
+
 for(value in arr){
-    console.log(arr[value])
+    console.log(arr[value]);
 }
+
 console.log("__________________________");
+
 for(value of arr){
-    console.log(value)
+    console.log(value);
 }
+
 console.log("__________________________");
-arr.forEach((value => console.log(value)
-))
+
+arr.forEach(value => console.log(value)
+)
